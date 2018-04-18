0455a4bdc9ba551006ad989b2b15181e3017267d
Liu Xinwu <xinwu.liu@intel.com>
2018-04-18 10:02:00 +0800

    DM:tools: channel module for acrn-crashlog/acrnprobe
    The channel represents a way of detecting the system's events.
    So far, there are 3 channels:
    1. oneshot, detect once while acrnprobe startup.
    2. polling, run a detecting job with fixed time interval.
    3. inotify, watch the change of file or dir.
    
    Signed-off-by: Liu Xinwu <xinwu.liu@intel.com>

---
+++ tools/acrn-crashlog/acrnprobe/include/channels.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _CHANNELS_H
+#define _CHANNELS_H
+
+#define BASE_DIR_MASK		(IN_CLOSE_WRITE | IN_DELETE_SELF | IN_MOVE_SELF)
+#define UPTIME_MASK		IN_CLOSE_WRITE
+#define MAXEVENTS 15
+#define HEART_RATE (6 * 1000) /* ms */
+
+struct channel_t {
+	char *name;
+	int fd;
+	void (*channel_fn)(char *);
+};
+int create_detached_thread(pthread_t *pid, void *(*fn)(void *), void *arg);
+int init_channels(void);
+
+
+#endif
