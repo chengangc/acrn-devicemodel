3bbfaf8aa23db6ce1099caf24986fd99ee31d73f
Liu Xinwu <xinwu.liu@intel.com>
2018-04-25 14:40:37 +0800

    DM:tools: main thread of acrn-crashlog/acrnprobe
    Main thread of acrnprobe.
    
    Signed-off-by: Liu Xinwu <xinwu.liu@intel.com>

---
+++ tools/acrn-crashlog/acrnprobe/include/event.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __CRASH_EVENT_H__
+#define __CRASH_EVENT_H__
+
+void init_event(void);
+
+#endif
